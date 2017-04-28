a79b325951698094d567d1701a68a5a96c8b705a
Lucas Rodrigues <lucasrdx@outlook.com>
2017-04-28 16:26:16 -0300

    First version working

--- src/main/java/br/com/emailsender/service/EmailService.java
+++ src/main/java/br/com/emailsender/service/EmailService.java
@@ -1,6 +1,5 @@
 package br.com.emailsender.service;
 
-import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
@@ -12,21 +11,17 @@ public class EmailService {
 	public void send(String name, String guestEmail) {
 		try{
 			Email email = new SimpleEmail();
-			email.setHostName("smtp.googlemail.com");
-			email.setSmtpPort(465);
-			email.setAuthenticator(new DefaultAuthenticator("email", "password"));
-			email.setSSLOnConnect(true);
-			
-			email.setFrom("email");
+			email.setHostName("smtp-mail.outlook.com");
+			email.setSmtpPort(587);
+			email.setStartTLSEnabled(true);
+			email.setAuthentication("email", "password");
+			email.setFrom("email", "yourname");
 			email.setSubject("You just have been invited by VIPList!");
 	        email.setMsg("Hello " + name + ". You just have been invited by VIPList!");
 	        email.addTo(guestEmail);
 	        email.send();
-			
 		} catch (EmailException e) {
 			e.printStackTrace();
 		}
-		
-		
 	}
 }
